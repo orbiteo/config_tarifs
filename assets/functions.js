bb0ee0234bdd0603bf3296007d0a5be143f85572
Eloïse Schmitt <eloiseschmitt@MacBook-Pro-de-Eloise.local>
2018-12-28 07:42:16 +0100

    création fichier js + déplacement auto slides

---
+++ assets/functions.js
@@ -0,0 +1,119 @@
+	var slider1 = $("#variable1");
+	slider1.slider();
+	var slider2 = $("#variable2");
+	slider2.slider();
+	var slider3 = $("#variable3");
+	slider3.slider();
+	var slider4 = $("#variable4");
+	slider4.slider();
+
+	slider1.on('change', function() {
+		var valeur = $(this).slider('getValue');
+		$('.var1donnee').html(valeur);
+		var arrondie = Math.round((valeur/0.1)/70);
+		$('.var2donnee').html(arrondie);
+		slider2.slider('setValue', arrondie);
+	});
+	
+	slider2.on('change', function() {
+		var valeur = $(this).slider('getValue');
+		$('.var2donnee').html(valeur);
+		var arrondie = Math.round((valeur*70)*0.1);
+		$('.var1donnee').html(arrondie);
+		slider1.slider('setValue', arrondie);
+	});
+	
+	slider3.on('change', function() {
+		var valeur = $(this).slider('getValue');
+		$('.var3donnee').html(valeur);
+		var arrondie = Math.round(valeur*0.2);
+		$('.var4donnee').html(arrondie);
+		slider4.slider('setValue', arrondie);
+	});
+	
+	slider4.on('change', function() {
+		var valeur = $(this).slider('getValue');
+		$('.var4donnee').html(valeur);
+		var arrondie = Math.round(valeur/0.2);
+		$('.var3donnee').html(arrondie);
+		slider3.slider('setValue', arrondie);
+	});
+
+	$('.minus').on('click', function() {
+		addOrRemove('remove', $(this));
+	});
+	$('.plus').on('click', function() {
+		addOrRemove('add', $(this));
+	});
+
+	$('#first').on('click', function() {
+		changeButtons($(this));
+		setValues(49, 7, 500, 100);
+	});
+	$('#second').on('click', function() {
+		changeButtons($(this));
+		setValues(98, 14, 1000, 200);
+	});
+	$('#third').on('click', function() {
+		changeButtons($(this));
+		setValues(196, 28, 2000, 400);
+	});
+	$('#fourth').on('click', function() {
+		changeButtons($(this));
+		setValues(392, 56, 3000, 600);
+	});
+
+	
+	function changeButtons(selector) {
+		selector.css('background-color', '#138496');
+		selector.css('color', '#fff');
+		selector.siblings().css('background-color', '#fff');
+		selector.siblings().css('color', '#138496');
+	}
+
+	function setValues(valeur1, valeur2, valeur3, valeur4) {
+		slider1.slider('setValue', valeur1);
+		slider2.slider('setValue', valeur2);
+		slider3.slider('setValue', valeur3);
+		slider4.slider('setValue', valeur4);
+		$('.var1donnee').html(valeur1);
+		$('.var2donnee').html(valeur2);
+		$('.var3donnee').html(valeur3);
+		$('.var4donnee').html(valeur4);
+	}
+
+	function addOrRemove(symbole, selector) {
+		var nouvelleValeur;
+		var input = selector.parents('.slideOptions').children().children('.col-lg-5').children('input');
+		var valeur1 = input.slider('getValue');
+		if(symbole == 'add') {
+			nouvelleValeur = valeur1+1;
+		} else if(symbole == 'remove') {
+			nouvelleValeur = valeur1-1;
+		}
+		else {
+			nouvelleValeur = valeur1;
+		}
+		input.slider('setValue', nouvelleValeur);
+		var valeur = selector.parents('.slideOptions').children().children('.col-lg-3').children('span');
+		valeur.html(nouvelleValeur);
+
+		//Changement auto des autres valeurs
+		if(input.prop('id') == 'variable1') {
+			var arrondie = Math.round((nouvelleValeur/0.1)/70);
+			$('.var2donnee').html(arrondie);
+			slider2.slider('setValue', arrondie);
+		} else if(input.prop('id') == 'variable2') {
+			var arrondie = Math.round((nouvelleValeur*70)*0.1);
+			$('.var1donnee').html(arrondie);
+			slider1.slider('setValue', arrondie);
+		} else if(input.prop('id') == 'variable3') {
+			var arrondie = Math.round(nouvelleValeur*0.2);
+			$('.var4donnee').html(arrondie);
+			slider4.slider('setValue', arrondie);
+		} else if(input.prop('id') == 'variable4') {
+			var arrondie = Math.round(nouvelleValeur/0.2);
+			$('.var3donnee').html(arrondie);
+			slider3.slider('setValue', arrondie);
+		} 
+	}
--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 				<div class="col-lg-1 text-center minus"><a href="#"> - </a></div>
 				<div class="col-lg-5"><input id="variable2" data-slider-id='ex2Slider' type="text" data-slider-min="7" data-slider-max="100" data-slider-step="1" data-slider-value="28"/></div>
 				<div class="col-lg-1 text-center plus"><a href="#"> + </a></div>
-				<div class="col-lg-3 valeur"><span class="var2donnee">128</span>h/mois</div>
+				<div class="col-lg-3 valeur"><span class="var2donnee">28</span>h/mois</div>
 			</div>
 		</div>
 		<div class="slideOptions">
@@ -59,91 +59,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="assets/bootstrap-slider.min.js"></script>
-<script type="text/javascript">
-	var slider1 = $("#variable1");
-	slider1.slider();
-	slider1.on('change', function() {
-		var valeur1 = slider1.slider('getValue');
-		$('.var1donnee').html(valeur1);
-	});
-	var slider2 = $("#variable2");
-	slider2.slider();
-	slider2.on('change', function() {
-		var valeur2 = slider2.slider('getValue');
-		$('.var2donnee').html(valeur2);
-	});
-	var slider3 = $("#variable3");
-	slider3.slider();
-	slider3.on('change', function() {
-		var valeur3 = slider3.slider('getValue');
-		$('.var3donnee').html(valeur3);
-	});
-	var slider4 = $("#variable4");
-	slider4.slider();
-	slider4.on('change', function() {
-		var valeur4 = slider4.slider('getValue');
-		$('.var4donnee').html(valeur4);
-	});
-
-	$('.minus').on('click', function() {
-		addOrRemove('remove', $(this));
-	});
-	$('.plus').on('click', function() {
-		addOrRemove('add', $(this));
-	});
-
-	$('#first').on('click', function() {
-		changeButtons($(this));
-		setValues(49, 7, 500, 100);
-	});
-	$('#second').on('click', function() {
-		changeButtons($(this));
-		setValues(98, 14, 1000, 200);
-	});
-	$('#third').on('click', function() {
-		changeButtons($(this));
-		setValues(196, 28, 2000, 400);
-	});
-	$('#fourth').on('click', function() {
-		changeButtons($(this));
-		setValues(392, 56, 3000, 600);
-	});
-
-	function changeButtons(selector) {
-		selector.css('background-color', '#138496');
-		selector.css('color', '#fff');
-		selector.siblings().css('background-color', '#fff');
-		selector.siblings().css('color', '#138496');
-	}
-
-	function setValues(valeur1, valeur2, valeur3, valeur4) {
-		slider1.slider('setValue', valeur1);
-		slider2.slider('setValue', valeur2);
-		slider3.slider('setValue', valeur3);
-		slider4.slider('setValue', valeur4);
-		$('.var1donnee').html(valeur1);
-		$('.var2donnee').html(valeur2);
-		$('.var3donnee').html(valeur3);
-		$('.var4donnee').html(valeur4);
-	}
-
-	function addOrRemove(symbole, selector) {
-		var nouvelleValeur;
-		var input = selector.parents('.slideOptions').children().children('.col-lg-5').children('input');
-		var valeur1 = input.slider('getValue');
-		if(symbole == 'add') {
-			nouvelleValeur = valeur1+1;
-		} else if(symbole == 'remove') {
-			nouvelleValeur = valeur1-1;
-		}
-		else {
-			nouvelleValeur = valeur1;
-		}
-		input.slider('setValue', nouvelleValeur);
-		var valeur = selector.parents('.slideOptions').children().children('.col-lg-3');
-		valeur.html(nouvelleValeur);
-	}
-</script>
+<script type="text/javascript" src="assets/functions.js"></script>
 
 </body>
 
