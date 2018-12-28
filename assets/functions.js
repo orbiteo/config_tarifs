d4100cbd81ce6eeafdf0ed30c6e42413f763c82d
Eloïse Schmitt <eloiseschmitt@MacBook-Pro-de-Eloise.local>
2018-12-28 15:53:05 +0100

    Modif variables et calculs

--- assets/functions.js
+++ assets/functions.js
@@ -1,88 +1,125 @@
-	var slider1 = $("#variable1");
-	slider1.slider();
-	var slider2 = $("#variable2");
-	slider2.slider();
-	var slider3 = $("#variable3");
-	slider3.slider();
-	var slider4 = $("#variable4");
-	slider4.slider();
+	var slider = {};
+	for(var a=1; a<=9 ; a++) {
+		slider[a] = $("#variable"+a);
+		slider[a].slider();
+	}
 
-	slider1.on('change', function() {
+	slider[1].on('change', function() {
 		var valeur = $(this).slider('getValue');
 		$('.var1donnee').html(valeur);
 		var arrondie = Math.round((valeur/0.1)/70);
 		$('.var2donnee').html(arrondie);
-		slider2.slider('setValue', arrondie);
+		slider[2].slider('setValue', arrondie);
 	});
 	
-	slider2.on('change', function() {
-		var valeur = $(this).slider('getValue');
-		$('.var2donnee').html(valeur);
-		var arrondie = Math.round((valeur*70)*0.1);
-		$('.var1donnee').html(arrondie);
-		slider1.slider('setValue', arrondie);
-	});
-	
-	slider3.on('change', function() {
+	slider[3].on('change', function() {
 		var valeur = $(this).slider('getValue');
 		$('.var3donnee').html(valeur);
 		var arrondie = Math.round(valeur*0.2);
 		$('.var4donnee').html(arrondie);
-		slider4.slider('setValue', arrondie);
+		slider[4].slider('setValue', arrondie);
 	});
 	
-	slider4.on('change', function() {
+	slider[4].on('change', function() {
 		var valeur = $(this).slider('getValue');
 		$('.var4donnee').html(valeur);
 		var arrondie = Math.round(valeur/0.2);
 		$('.var3donnee').html(arrondie);
-		slider3.slider('setValue', arrondie);
+		slider[3].slider('setValue', arrondie);
 	});
+
+	slider[5].on('change', function() {
+		var valeur = $(this).slider('getValue');
+		$(".var5donnee").html(valeur);
+		totalTechCrea();
+	});
+	slider[6].on('change', function() {
+		var valeur = $(this).slider('getValue');
+		$(".var6donnee").html(valeur);
+		totalTechCrea();
+	});
+	slider[7].on('change', function() {
+		var valeur = $(this).slider('getValue');
+		$(".var7donnee").html(valeur);
+		totalTechCrea();
+	});
+	slider[8].on('change', function() {
+		var valeur = $(this).slider('getValue');
+		$(".var8donnee").html(valeur);
+		totalTechCrea();
+	});
+	slider[9].on('change', function() {
+		var valeur = $(this).slider('getValue');
+		$(".var9donnee").html(valeur);
+		totalTechCrea();
+	});
+	
+	
 
 	$('.minus').on('click', function() {
 		addOrRemove('remove', $(this));
+		totalTechCrea();
 	});
 	$('.plus').on('click', function() {
 		addOrRemove('add', $(this));
+		totalTechCrea();
 	});
 
 	$('#first').on('click', function() {
 		changeButtons($(this));
-		setValues(49, 7, 500, 100);
+		setValues(0.6, 0, 0, 2, 0, 1, 1, 2);
 	});
 	$('#second').on('click', function() {
 		changeButtons($(this));
-		setValues(98, 14, 1000, 200);
+		setValues(1.2, 1000, 100, 4, 0, 2, 2, 4);
 	});
 	$('#third').on('click', function() {
 		changeButtons($(this));
-		setValues(196, 28, 2000, 400);
+		setValues(2.9, 1000, 100, 10, 4, 4, 4, 7);
 	});
 	$('#fourth').on('click', function() {
 		changeButtons($(this));
-		setValues(392, 56, 3000, 600);
+		setValues(3.6, 2000, 200, 14, 4, 4, 4, 10);
 	});
 
-	
-	function changeButtons(selector) {
+	function totalTechCrea() { //Calcul du temps "stratégie & gestion"
+		var total = 0;
+		for(var a=5 ; a<10 ; a++) {
+			total += slider[a].slider('getValue');
+		}
+		var nouvelleVar1 = total*0.1;
+		slider[1].slider('setValue', nouvelleVar1);
+		var arrondie = nouvelleVar1.toFixed(1);
+		$('.var1donnee').html(arrondie);
+	}
+
+	function changeButtons(selector) { // Apparence boutons haut de page
 		selector.css('background-color', '#0065CB');
 		selector.css('color', '#fff');
 		selector.siblings().css('background-color', '#fff');
 		selector.siblings().css('color', '#0065CB');
 	}
 
-	function setValues(valeur1, valeur2, valeur3, valeur4) {
-		slider1.slider('setValue', valeur1);
-		slider2.slider('setValue', valeur2);
-		slider3.slider('setValue', valeur3);
-		slider4.slider('setValue', valeur4);
+	function setValues(valeur1, valeur3, valeur4, valeur5, valeur6, valeur7, valeur8, valeur9 ) { // Valeurs auto suivant formule choisie
+		slider[1].slider('setValue', valeur1);
+		slider[3].slider('setValue', valeur3);
+		slider[4].slider('setValue', valeur4);
+		slider[5].slider('setValue', valeur5);
+		slider[6].slider('setValue', valeur6);
+		slider[7].slider('setValue', valeur7);
+		slider[8].slider('setValue', valeur8);
+		slider[9].slider('setValue', valeur9);
 		$('.var1donnee').html(valeur1);
-		$('.var2donnee').html(valeur2);
 		$('.var3donnee').html(valeur3);
 		$('.var4donnee').html(valeur4);
+		$('.var5donnee').html(valeur5);
+		$('.var6donnee').html(valeur6);
+		$('.var7donnee').html(valeur7);
+		$('.var8donnee').html(valeur8);
+		$('.var9donnee').html(valeur9);
 	}
 
-	function addOrRemove(symbole, selector) {
+	function addOrRemove(symbole, selector) { // Modifs valeurs si + ou - cliqué
 		var nouvelleValeur;
 		var input = selector.parents('.slideOptions').children().children('.col-lg-5').children('input');
 		var valeur1 = input.slider('getValue');
@@ -102,18 +139,14 @@
 		if(input.prop('id') == 'variable1') {
 			var arrondie = Math.round((nouvelleValeur/0.1)/70);
 			$('.var2donnee').html(arrondie);
-			slider2.slider('setValue', arrondie);
-		} else if(input.prop('id') == 'variable2') {
-			var arrondie = Math.round((nouvelleValeur*70)*0.1);
-			$('.var1donnee').html(arrondie);
-			slider1.slider('setValue', arrondie);
+			slider[2].slider('setValue', arrondie);
 		} else if(input.prop('id') == 'variable3') {
 			var arrondie = Math.round(nouvelleValeur*0.2);
 			$('.var4donnee').html(arrondie);
-			slider4.slider('setValue', arrondie);
+			slider[4].slider('setValue', arrondie);
 		} else if(input.prop('id') == 'variable4') {
 			var arrondie = Math.round(nouvelleValeur/0.2);
 			$('.var3donnee').html(arrondie);
-			slider3.slider('setValue', arrondie);
+			slider[3].slider('setValue', arrondie);
 		} 
 	}
--- index.php
+++ index.php
@@ -17,17 +17,62 @@
 			<button id="third" type="button" class="btn btn-orbiteo">3ème choix</button>
 			<button id="fourth" type="button" class="btn btn-orbiteo">4ème choix</button>	
 		</div>
-		
+
 		<div class="slideOptions">
 			<div class="row">
-				<div class="col-lg-2">Stratégie</div>
+				<div class="col-lg-2">Marketing digital</div>
 				<div class="col-lg-1 text-center minus"><a href="#" class="symbol"> - </a></div>
-				<div class="col-lg-5"><input id="variable1" data-slider-id='ex1Slider' type="text" data-slider-min="0" data-slider-max="700" data-slider-step="10" data-slider-value="196" data-slider-handle="custom"/></div>
+				<div class="col-lg-5"><input id="variable9" data-slider-id='ex9Slider' type="text" data-slider-min="0" data-slider-max="20" data-slider-step="1" data-slider-value="7" data-slider-handle="custom"/></div>
 				<div class="col-lg-1 text-center plus"><a href="#" class="symbol"> + </a></div>
-				<div class="col-lg-3 valeur"><span class="var1donnee">196</span>,00 €</div>
+				<div class="col-lg-3 valeur"><span class="var9donnee">7</span>h/mois</div>
 			</div>
 		</div>
 		<div class="slideOptions">
+			<div class="row">
+				<div class="col-lg-2">Développement web</div>
+				<div class="col-lg-1 text-center minus"><a href="#" class="symbol"> - </a></div>
+				<div class="col-lg-5"><input id="variable8" data-slider-id='ex8Slider' type="text" data-slider-min="0" data-slider-max="20" data-slider-step="1" data-slider-value="4" data-slider-handle="custom"/></div>
+				<div class="col-lg-1 text-center plus"><a href="#" class="symbol"> + </a></div>
+				<div class="col-lg-3 valeur"><span class="var8donnee">4</span>h/mois</div>
+			</div>
+		</div>
+		<div class="slideOptions">
+			<div class="row">
+				<div class="col-lg-2">Studio graphique</div>
+				<div class="col-lg-1 text-center minus"><a href="#" class="symbol"> - </a></div>
+				<div class="col-lg-5"><input id="variable7" data-slider-id='ex7Slider' type="text" data-slider-min="0" data-slider-max="20" data-slider-step="1" data-slider-value="4" data-slider-handle="custom"/></div>
+				<div class="col-lg-1 text-center plus"><a href="#" class="symbol"> + </a></div>
+				<div class="col-lg-3 valeur"><span class="var7donnee">4</span>h/mois</div>
+			</div>
+		</div>
+		<div class="slideOptions">
+			<div class="row">
+				<div class="col-lg-2">Accompagnement CRM</div>
+				<div class="col-lg-1 text-center minus"><a href="#" class="symbol"> - </a></div>
+				<div class="col-lg-5"><input id="variable6" data-slider-id='ex6Slider' type="text" data-slider-min="0" data-slider-max="20" data-slider-step="1" data-slider-value="4" data-slider-handle="custom"/></div>
+				<div class="col-lg-1 text-center plus"><a href="#" class="symbol"> + </a></div>
+				<div class="col-lg-3 valeur"><span class="var6donnee">4</span>h/mois</div>
+			</div>
+		</div>
+		<div class="slideOptions">
+			<div class="row">
+				<div class="col-lg-2">Référencement naturel</div>
+				<div class="col-lg-1 text-center minus"><a href="#" class="symbol"> - </a></div>
+				<div class="col-lg-5"><input id="variable5" data-slider-id='ex5Slider' type="text" data-slider-min="0" data-slider-max="20" data-slider-step="1" data-slider-value="10" data-slider-handle="custom"/></div>
+				<div class="col-lg-1 text-center plus"><a href="#" class="symbol"> + </a></div>
+				<div class="col-lg-3 valeur"><span class="var5donnee">10</span>h/mois</div>
+			</div>
+		</div>
+		<div class="slideOptions">
+			<div class="row">
+				<div class="col-lg-2">Stratégie & Gestion de projet</div>
+				<div class="col-lg-1 text-center minus"><a href="#" class="symbol"> - </a></div>
+				<div class="col-lg-5"><input id="variable1" data-slider-id='ex1Slider' type="text" data-slider-min="0" data-slider-max="20" data-slider-step="0.1" data-slider-value="2.9" data-slider-handle="custom" data-slider-enabled="false"/></div>
+				<div class="col-lg-1 text-center plus"><a href="#" class="symbol"> + </a></div>
+				<div class="col-lg-3 valeur"><span class="var1donnee">2.9</span>h/mois</div>
+			</div>
+		</div>
+		<!-- <div class="slideOptions">
 			<div class="row">
 				<div class="col-lg-2">Volume horaire</div>
 				<div class="col-lg-1 text-center minus"><a href="#" class="symbol"> - </a></div>
@@ -35,23 +80,23 @@
 				<div class="col-lg-1 text-center plus"><a href="#" class="symbol"> + </a></div>
 				<div class="col-lg-3 valeur"><span class="var2donnee">28</span>h/mois</div>
 			</div>
-		</div>
+		</div> -->
 		<div class="slideOptions">
 			<div class="row">
-				<div class="col-lg-2">Pub</div>
+				<div class="col-lg-2">Bugdet Pub</div>
 				<div class="col-lg-1 text-center minus"><a href="#" class="symbol"> - </a></div>
 				<div class="col-lg-5"><input id="variable3" data-slider-id='ex3Slider' type="text" data-slider-min="500" data-slider-max="10000" data-slider-step="100" data-slider-value="2000" data-slider-handle="custom"/></div>
 				<div class="col-lg-1 text-center plus"><a href="#" class="symbol"> + </a></div>
-				<div class="col-lg-3 valeur"><span class="var3donnee">2000</span>,00 €</div>
+				<div class="col-lg-3 valeur"><span class="var3donnee">1000</span>,00 €</div>
 			</div>
 		</div>
 		<div class="slideOptions">
 			<div class="row">
-				<div class="col-lg-2">Achat</div>
+				<div class="col-lg-2">Achat (BL, Articles)</div>
 				<div class="col-lg-1 text-center minus"><a href="#" class="symbol"> - </a></div>
-				<div class="col-lg-5"><input id="variable4" data-slider-id='ex4Slider' type="text" data-slider-min="100" data-slider-max="2000" data-slider-step="1" data-slider-value="400" data-slider-handle="custom"/></div>
+				<div class="col-lg-5"><input id="variable4" data-slider-id='ex4Slider' type="text" data-slider-min="100" data-slider-max="2000" data-slider-step="1" data-slider-value="100" data-slider-handle="custom"/></div>
 				<div class="col-lg-1 text-center plus"><a href="#" class="symbol"> + </a></div>
-				<div class="col-lg-3 valeur"><span class="var4donnee">400</span>,00 €</div>
+				<div class="col-lg-3 valeur"><span class="var4donnee">100</span>,00 €</div>
 			</div>
 		</div>
 	</div>
